1349135c367b32b5695ebeda6dd5e4ba01782757
Leo <volwahsenl@gmail.com>
2023-07-30 08:28:24 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { list } from '@angular/fire/database';
+import { environment } from './environments/environment.development';
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +20,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDsGKUuoVaLhddVN1aPr66EnnNY1AoFwNo",
+        authDomain: "angular-blog-c2e8e.firebaseapp.com",
+        projectId: "angular-blog-c2e8e",
+        storageBucket: "angular-blog-c2e8e.appspot.com",
+        messagingSenderId: "1078559179035",
+        appId: "1:1078559179035:web:48e73a94bbdec3a2c113be"
+    }
+};
